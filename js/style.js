1fea65959c1a02de0f6a22d4469d901f9a53dfd4
kieukhang185 <71631430+kieukhang185@users.noreply.github.com>
2021-07-22 13:47:28 +0700

    add file

---
+++ js/style.js
@@ -0,0 +1,24 @@
+const hamburer = document.querySelector(".hamburger");
+const navList = document.querySelector(".menu-list");
+
+if (hamburer) {
+  hamburer.addEventListener("click", () => {
+    navList.classList.toggle("open");
+  });
+}
+
+
+// Hero carousel
+const slider1 = document.querySelector("#glide1");
+if (slider1) {
+  new Glide(slider1, {
+    type: "carousel",
+    startAt: 0,
+    autoplay: 5000,
+    gap: 0,
+    hoverpause: true,
+    perView: 1,
+    animationDuration: 800,
+    animationTimingFunc: "linear",
+  }).mount();
+}
