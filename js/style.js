bcb20ef8a09d7a10aec9a8381db87b895b7e7f0b
kieukhang185 <71631430+kieukhang185@users.noreply.github.com>
2021-08-06 14:14:17 +0700

    edit 6/8 14h

--- js/style.js
+++ js/style.js
@@ -8,17 +8,17 @@ if (hamburer) {
 }
 
 
-// Hero carousel
-const slider1 = document.querySelector("#glide1");
-if (slider1) {
-  new Glide(slider1, {
-    type: "carousel",
-    startAt: 0,
-    autoplay: 6000,
-    gap: 0,
-    hoverpause: true,
-    perView: 1,
-    animationDuration: 800,
-    animationTimingFunc: "linear",
-  }).mount();
-}
+// // Hero carousel
+// const slider1 = document.querySelector("#glide1");
+// if (slider1) {
+//   new Glide(slider1, {
+//     type: "carousel",
+//     startAt: 0,
+//     autoplay: 6000,
+//     gap: 0,
+//     hoverpause: true,
+//     perView: 1,
+//     animationDuration: 800,
+//     animationTimingFunc: "linear",
+//   }).mount();
+// }
